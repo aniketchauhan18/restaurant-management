bf15ae400a01f803b1e7f025f060d4d68554c3c6
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-10 21:32:39 +0530

    input-validation middleware added

--- backend/routes/v1/user.routes.js
+++ backend/routes/v1/user.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const { register, login } = require('../../controllers/user.controller')
+const userRegisterationValidation = require('../../middlewares/user/userRegisterationValidation');
+const userLoginValidation = require('../../middlewares/user/userLoginValidation');
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/register').post(userRegisterationValidation, register);
+router.route('/login').post(userLoginValidation, login);
 
 module.exports = router;
