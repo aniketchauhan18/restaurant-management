d01538594b1778b04b604a95087f23942be87e89
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-10 21:32:55 +0530

    input-validation middleware added

--- backend/routes/v1/restaurant.routes.js
+++ backend/routes/v1/restaurant.routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const { register, deleteRestaurant, updateRestaurant, getRestaurant, getRestaurantById } = require('../../controllers/restaurant.controller');
-const adminMiddleWare = require('../../middlewares/adminMiddleware');
-const jwtAuth = require('../../middlewares/jwtAuth');
+const adminMiddleWare = require('../../middlewares/admin/adminMiddleware');
+// const jwtAuth = require('../../middlewares/jwtAuth');
+const jwtAuth = require('../../middlewares/auth/jwtAuth');
+const restaurantRegisterationValidation = require('../../middlewares/admin/restaurantMiddleware');
 
 router.route('/').get(getRestaurant);
-router.route('/register').post(jwtAuth, adminMiddleWare, register);
+router.route('/register').post(jwtAuth, adminMiddleWare,restaurantRegisterationValidation, register);
 router.route('/admin/:id').get(getRestaurantById);
 router.route('/update/:restaurantId').patch(jwtAuth, adminMiddleWare, updateRestaurant);
 router.route('/delete/:restaurantId').delete( jwtAuth, adminMiddleWare, deleteRestaurant );
