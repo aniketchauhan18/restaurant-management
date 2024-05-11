d501e117df19049e1f91cfafbe7fcbcc60b0aaf9
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-11 11:38:15 +0530

    added utility functions

---
+++ backend/utils/adminUtils/menuUtils.js
@@ -0,0 +1,35 @@
+const Menu = require('../../models/menu.models')
+
+const menuExists = async(name) => {
+  const menu = await Menu.findOne({name});
+  if (menu) return true
+  return false
+}
+
+const menuCreate = async(newMenu) => {
+  const menu = await Menu.create(newMenu)
+  return menu
+}
+
+const menuFind = async(restaurantId) => {
+  const menu = await Menu.find({restaurantId})
+  return menu
+}
+
+const menuUpdate = async(id, data) => {
+  const updatedMenu = await Menu.findOne(id, data, {runValidators: true})
+  return updatedMenu
+}
+
+const menuDelete = async(id) => {
+  const deletedMenu = await Menu.findByIdAndDelete(id)
+  return deletedMenu
+}
+
+module.exports = {
+  menuExists,
+  menuCreate,
+  menuUpdate,
+  menuDelete,
+  menuFind
+}
