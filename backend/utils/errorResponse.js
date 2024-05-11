bf6675c54c7398b7ad6b75ebeb732593797b84a7
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-11 11:41:32 +0530

    more functions added

--- backend/utils/errorResponse.js
+++ backend/utils/errorResponse.js
@@ -6,27 +6,48 @@ const sendSuccess = (res, statusCode, message) => {
   return res.status(statusCode).json({message})
 }
 
+const sendResponse = (res, statusCode, data) => {
+  return res.status(statusCode).json({data})
+}
+
 const InternalServerError = (res, message = "Internal server error") => {
   return sendError(res, 500, message)
 }
 
 const InvalidRequestBody = (res, message = "Invalid request body") => {
+  console.log(message)
   return sendError(res, 400, message)
 }
 
 const entityAlreadyExists = (res, entity) => {
   const message = `${entity} already exists`
+  console.log(message)
   return sendError(res, 400, message)
 }
 
 const entityCreatedSuccessfully = (res, entity) => {
   const message = `${entity} created successfully`
+  console.log("hhoooo")
   return sendSuccess(res, 200, message)
 }
 
 const entityNotExist = (res, entity) => {
   const message = `${entity} doesn't exist`
-  return sendSuccess(res, 404, message)
+  return sendError(res, 404, message)
+}
+
+const entityDeletedSucessfully = (res, entity) => {
+  const message = `${entity} deleted sucessfully`
+  return sendSuccess(res, 200, message)
+}
+
+const entityUpdatedSuccessfully = (res, entity) => {
+  const message = `${entity} updated Successfully`
+  return sendSuccess(res, 200, message)
+}
+
+const getEntityReponse = (res, data) => {
+  return sendResponse(res, 200, data)
 }
 
 module.exports = {
@@ -34,5 +55,8 @@ module.exports = {
   InvalidRequestBody,
   entityAlreadyExists,
   entityCreatedSuccessfully,
-  entityNotExist
+  entityNotExist,
+  entityDeletedSucessfully,
+  entityUpdatedSuccessfully,
+  getEntityReponse
 }
