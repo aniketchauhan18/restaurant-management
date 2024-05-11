f0b1b59d5c4ba9f4e1d57af2cd1870e8a11673d8
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-11 11:36:11 +0530

    menu-data validation

---
+++ backend/middlewares/admin/menuMiddleware.js
@@ -0,0 +1,17 @@
+const { menuValidationSchema } = require('../../auth/schemas/menu.schema')
+const { InternalServerError, InvalidRequestBody } = require('../../utils/errorResponse')
+
+const menuRegisterationValidation = (req, res, next) => {
+  try {
+    const { success, data } = menuValidationSchema.safeParse(req.body)
+    
+    req.menuValidatedData = data
+
+    if (!success) return InvalidRequestBody(res)
+    next()
+  } catch (err) {
+    return InternalServerError(res)
+  }
+}
+
+module.exports = menuRegisterationValidation;
