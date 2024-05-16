46f9cd9a20c9d24e5c5d7897aa20d4162d402d5a
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-16 17:38:48 +0530

    added styling

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { IoLogoGithub,IoLogoLinkedin } from "react-icons/io";
+import { RiTwitterXFill } from "react-icons/ri";
 
 function Footer() {
+  const linkClasses = "text-2xl transform  duration-200 hover:scale-110";
+
   return (
-    <div>Footer</div>
+    <footer
+      className="flex flex-col mt-12"
+    >
+      <div className="flex flex-col">
+        <div className="flex justify-evenly md:self-start md:gap-10 md:pl-5">
+          <Link >
+            <IoLogoGithub className={linkClasses}/>
+          </Link>
+          <Link className={linkClasses}>
+            <IoLogoLinkedin />
+          </Link>
+          <Link className={linkClasses}>
+            <RiTwitterXFill />
+          </Link>
+        </div>
+      </div>
+      <small className="flex justify-center my-3 text-sm">
+        Made with ❤️ by Aniket Chauhan
+      </small>
+    </footer>
   )
 }
 
--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom"
 
 function Header() {
   // const [showLogin , setShowLogin] = useState(true);
-  const [authenticatedUser, setAuthenticatedUser] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const linkClasses = "bg-scarlet-400 text-white py-1 px-2 rounded-sm hover:bg-scarlet-500 transition ease-in-out duration-300 font-lato"
+  const linkClasses = "text-scarlet-400 py-1 px-1 hover:text-scarlet-500 transition ease-in-out duration-300 font-mukta nav-links"
 
   useEffect(() => {
-    const userExists = localStorage.getItem("userId");
+    const userExists = localStorage.getItem("jwtToken");
+    console.log(userExists)
     if (userExists){
-      setAuthenticatedUser((prev) => !prev)
+      setIsLoggedIn(true)
     }
     // } else {
-    //   setAuthenticatedUser(true)
+    //   setisLoggedIn(true)
     // }
   }, [])
 
@@ -22,33 +23,42 @@ function Header() {
   // }
 
   const handleLogout = () => {
-    localStorage.clear("jwtToken")
-    localStorage.clear("userId")
+    localStorage.removeItem("jwtToken")
+    localStorage.removeItem("userId")
+    setIsLoggedIn(false)
   }
 
 
   return (
-      <nav className='flex px-3 items-center sticky w-full h-12 border-b py-4'>
-        <h1>
+    <div className="flex justify-center mb-10">
+      <nav 
+        className='flex px-3 items-center w-full h-12 border-b py-4  border border-zinc-200 rounded-full m-5 justify-between'
+      >
+        <Link to={'/'} className="font-poppins text-2xl restaurant-heading flex justify-center items-center">
           Restaurant
-        </h1>
-        <ul className='flex sticky justify-left gap-3 ml-auto mr-5'>
-          <Link className={linkClasses}>
-            About
+        </Link>
+        <div className="flex justify-center w-full items-center">
+          <div className='flex gap-5 ml-auto mr-5 nav-container'>
+          <Link className={linkClasses} to={'/createrestaurant'}>
+            Add Restaurants
           </Link>
-          <div className="flex justify-center">
-            {authenticatedUser ? 
-              <Link to={'/login'} className={linkClasses}>
-                Login
-              </Link>
-              : 
+          <Link className={linkClasses} to={'/login'} >
+            LogIn
+          </Link>
+          <Link className={linkClasses} to={'/register'}>
+            SignUp
+          </Link>
+          {/* <div className="flex justify-center">
+            {isLoggedIn &&
               <Link to={'/'} onClick={handleLogout} className={linkClasses}>
                 Logout
               </Link>
             }
+          </div> */}
           </div>
-        </ul>
+        </div>
       </nav>
+    </div>
   )
 }
 
