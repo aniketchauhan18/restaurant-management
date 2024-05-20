8d5aca3c77b79985b3126ad3ad8495e0457941d6
aniketchauhan18 <workwithaniket18@gmail.com>
2024-05-20 13:37:31 +0530

    ui refactored

--- frontend/src/components/user/User.jsx
+++ frontend/src/components/user/User.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import RestaurantCard from '../common/RestaurantCard'
+import { Link, useParams } from 'react-router-dom'
 
 function User() {
   const [restaurants, setRestaurants] = useState([]);
-  const indexRef = useRef();
   /*
     const indexRef = useRef()
 
@@ -34,44 +34,21 @@ function User() {
       const response = await fetch("http://localhost:3000/api/v1/restaurants/");
       const responseData = await response.json();
       console.log(responseData.data);
-      const restnts = responseData.data;
-      console.log(restnts);
-      const restaurantId = restnts.map((restaurant) => restaurant._id);
-      console.log(restaurantId);
-      const userRestaurantsIdArray = [restaurantId];
-      localStorage.setItem("userRestaurantsIds", userRestaurantsIdArray);
       setRestaurants(responseData.data);
     };
     fetchData();
   }, []);
 
-  const handleItemClick = (index) => {
-    indexRef.current = index;
-    localStorage.setItem("userCurrentRestaurant", index);
-  };
+  const { id } = useParams()
 
-  const mappedRestaurants = restaurants.map((restaurant, index) => {
+  const mappedRestaurants = restaurants.map((restaurant) => {
     return (
       <Link
-        to={"/menu"}
-        key={index}
+      to={`/user/${id}/${restaurant._id}/menu`}
+        key={restaurant._id}
         className="flex basis-1/5 transition duration-200 border border-zinc-100 ease-in-out hover:shadow rounded p-3 flex-col restaurant-card h-auto"
       >
-        <div className=" flex flex-1 justify-center items-center w-full mb-1">
-          <div className="w-full justify-center items-center">
-            <img
-              src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="w-full rounded"
-            />
-          </div>
-        </div>
-        <p className="text-[1rem] text-zinc-700 font-medium">
-          {restaurant.name}
-        </p>
-        <p className="text-zinc-500">{restaurant.city}</p>
-        <div className="flex justify-between">
-          <p className="text-zinc-400 text-sm">{restaurant.address}</p>
-        </div>
+        <RestaurantCard restaurantData={restaurant} />
       </Link>
     );
   });
